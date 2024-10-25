c6400fd4dd68f962fa70b060b8603ae847847783
basant <basantamin@outlook.sa>
2024-10-25 06:32:14 +0300

    Initial Commit

---
+++ Recipe1/Recipe1App.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe1App.swift
+//  Recipe1
+//
+//  Created by basant amin bakir on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
